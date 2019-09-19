72449ff1b21cba7fefa0ac0464ccaa9e9e139de2
Rudi <rudix.lab@gmail.com>
2019-09-19 12:28:35 +0300

    c

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,116 +1,116 @@
 const path = require('path');
 
 module.exports = {
-  siteMetadata: {
-    title: 'Дух',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://duh.netlify.com', // full path to blog - no ending slash
-  },
-  mapping: {
-    'MarkdownRemark.frontmatter.author': 'AuthorYaml',
-  },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-29127191-2',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Enables Google Optimize Experiment ID
-        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'example.com',
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-adsense`,
-      options: {
-        publisherId: `ca-pub-5476404733919333`,
-      },
-    },
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'content',
-        path: path.join(__dirname, 'src', 'content'),
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
+     siteMetadata: {
+          title: 'Дух',
+          description: 'The professional publishing platform',
+          siteUrl: 'https://duh.netlify.com', // full path to blog - no ending slash
+     },
+     mapping: {
+          'MarkdownRemark.frontmatter.author': 'AuthorYaml',
+     },
+     plugins: [
           {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1rem',
-            },
+               resolve: 'gatsby-plugin-google-analytics',
+               options: {
+                    trackingId: 'UA-29127191-2',
+                    // Defines where to place the tracking script - `true` in the head and `false` in the body
+                    head: true,
+                    // Setting this parameter is optional
+                    anonymize: true,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    exclude: ['/preview/**', '/do-not-track/me/too/'],
+                    // Delays sending pageview hits on route update (in milliseconds)
+                    pageTransitionDelay: 0,
+                    // Enables Google Optimize using your container Id
+                    optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+                    // Enables Google Optimize Experiment ID
+                    experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+                    // Set Variation ID. 0 for original 1,2,3....
+                    variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+                    // Any additional optional fields
+                    sampleRate: 5,
+                    siteSpeedSampleRate: 10,
+                    cookieDomain: 'example.com',
+               },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          'gatsby-remark-abbr',
           {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1170,
-              quality: 90,
-            },
+               resolve: 'gatsby-plugin-google-adsense',
+               options: {
+                    publisherId: 'ca-pub-5476404733919333',
+               },
           },
-        ],
-      },
-    },
-    'gatsby-transformer-json',
-    {
-      resolve: 'gatsby-plugin-canonical-urls',
-      options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
-      },
-    },
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-typescript',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-yaml',
-    'gatsby-plugin-feed',
-    {
-      resolve: 'gatsby-plugin-postcss',
-      options: {
-        postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-XXXX-Y',
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // IP anonymization for GDPR compliance
-        anonymize: true,
-        // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Specifies what percentage of users should be tracked
-        sampleRate: 100,
-        // Determines how often site speed tracking beacons will be sent
-        siteSpeedSampleRate: 10,
-      },
-    },
-  ],
+          'gatsby-plugin-sitemap',
+          'gatsby-plugin-sharp',
+          {
+               resolve: 'gatsby-source-filesystem',
+               options: {
+                    name: 'content',
+                    path: path.join(__dirname, 'src', 'content'),
+               },
+          },
+          {
+               resolve: 'gatsby-transformer-remark',
+               options: {
+                    plugins: [
+                         {
+                              resolve: 'gatsby-remark-responsive-iframe',
+                              options: {
+                                   wrapperStyle: 'margin-bottom: 1rem',
+                              },
+                         },
+                         'gatsby-remark-prismjs',
+                         'gatsby-remark-copy-linked-files',
+                         'gatsby-remark-smartypants',
+                         'gatsby-remark-abbr',
+                         {
+                              resolve: 'gatsby-remark-images',
+                              options: {
+                                   maxWidth: 1170,
+                                   quality: 90,
+                              },
+                         },
+                    ],
+               },
+          },
+          'gatsby-transformer-json',
+          {
+               resolve: 'gatsby-plugin-canonical-urls',
+               options: {
+                    siteUrl: 'https://gatsby-casper.netlify.com',
+               },
+          },
+          'gatsby-plugin-emotion',
+          'gatsby-plugin-typescript',
+          'gatsby-transformer-sharp',
+          'gatsby-plugin-react-helmet',
+          'gatsby-transformer-yaml',
+          'gatsby-plugin-feed',
+          {
+               resolve: 'gatsby-plugin-postcss',
+               options: {
+                    postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
+               },
+          },
+          {
+               resolve: 'gatsby-plugin-google-analytics',
+               options: {
+                    trackingId: 'UA-XXXX-Y',
+                    // Puts tracking script in the head instead of the body
+                    head: true,
+                    // IP anonymization for GDPR compliance
+                    anonymize: true,
+                    // Disable analytics for users with `Do Not Track` enabled
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    exclude: ['/preview/**'],
+                    // Specifies what percentage of users should be tracked
+                    sampleRate: 100,
+                    // Determines how often site speed tracking beacons will be sent
+                    siteSpeedSampleRate: 10,
+               },
+          },
+     ],
 };
