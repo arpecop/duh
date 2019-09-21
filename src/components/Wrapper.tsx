50f93f99ed6c8eec5534e63de0810a33f92e0c59
Rudi <rudix.lab@gmail.com>
2019-09-21 13:55:11 +0300

    c

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 
 const StyledWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
+     display: flex;
+     flex-direction: column;
+     min-height: 100vh;
 `;
 
 interface WrapperProps {
-  className?: string;
+     className?: string;
 }
 
 const Wrapper: React.FC<WrapperProps> = ({ children, className }) => (
-  <StyledWrapper className={className}>{children}</StyledWrapper>
+     <StyledWrapper className={className}>{children}</StyledWrapper>
 );
 
 export default Wrapper;
